9f623c3d41998d98905e64ef0f076bdd20f2ea4b
Sócrates Díaz <sdsxd1800@gmail.com>
2019-08-15 06:43:53 -0400

    Created homepage.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import LogoHeader from "../components/LogoHeader"
+import SearchBar from "../components/SearchBar"
+import CurrencyPrice from '../components/CurrencyPrice';
+import FuelPrice from '../components/FuelPrice';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <section className="hero is-info is-medium is-bold is-fullheight">
+                <div className="intro column is-8 is-offset-2">
+                    <LogoHeader />
+                    <SearchBar />
+                    <div className="columns">
+                        <CurrencyPrice className="column has-text-centered" />
+                        <FuelPrice className="column has-text-centered" />
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
