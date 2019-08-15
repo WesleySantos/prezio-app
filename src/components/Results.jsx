0436de088155edf7c747763c5156e422bb914341
Sócrates Díaz <sdsxd1800@gmail.com>
2019-08-15 17:47:09 -0400

    Showing ResultItem component.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import ResultItem from '../components/ResultItem';
 
 export default class Results extends Component {
     render() {
         return (
-            <div className={`box ${this.props.className}`} style={{margin: 5}}>
-                {this.props.matches.map((match) => {
-                    return (<div>{match.category}</div>)
+            <div className={`${this.props.className}`} style={{margin: 5}}>
+                {this.props.matches.map((match, index) => {
+                    return (
+                        <ResultItem result={match} key={index} />
+                    )
                 })}
             </div>
         )
